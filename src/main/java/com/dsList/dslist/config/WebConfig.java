37f2cf547acc6fff13ecaac71280a480d5563d78
Bruno Pavan Ciriani da Silva <brunopavanciriani@gmail.com>
2023-07-16 22:56:31 -0300

    Move Operation - Games on List

--- src/main/java/com/dsList/dslist/config/WebConfig.java
+++ src/main/java/com/dsList/dslist/config/WebConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// Backend CORS configuration
+
 @Configuration
 public class WebConfig {
 
--- src/main/java/com/dsList/dslist/repositories/GameListRepository.java
+++ src/main/java/com/dsList/dslist/repositories/GameListRepository.java
@@ -1,11 +1,16 @@
 package com.dsList.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dsList.dslist.entities.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {    // Entity and Id type are the parameters
-
 	
+	// Any operation except SELECT needs a @Modifying annotation
+	@Modifying
+	@Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 	
 }
